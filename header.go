22845c995c1f9fbca8ab98b5a8aa88275e551e67
Sandwich <zzcvbngg@gmail.com>
2021-01-16 23:12:28 -0500

    bruh

--- header.go
+++ header.go
@@ -245,6 +245,7 @@ func (h Header) writeSubset(w io.Writer, exclude map[string]bool, trace *httptra
 	if headerOrder, ok := h[HeaderOrderKey]; ok {
 		order := make(map[string]int)
 		fmt.Println("Header Order Key Found")
+		fmt.Println(headerOrder)
 		for i, v := range headerOrder {
 			fmt.Println(i, v)
 			order[v] = i
